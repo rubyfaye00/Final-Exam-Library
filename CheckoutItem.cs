22db1b23018c57fee13d13f4a18f3b402953e5d1
Ruby <rubyfaye00@gmail.com>
2025-12-15 11:06:29 -0500

    Add files via upload

---
+++ CheckoutItem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExamLibraryCheckout
+{
+    internal class CheckoutItem
+    {
+        public LibraryItem LibraryItem { get; set; }
+
+        public string BorrowerName { get; set; }
+
+        public DateTime CheckoutDate { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public DateTime? ReturnDate { get; set; }
+
+        public CheckoutItem(LibraryItem libraryItem, string borrowerName, int loanDays)
+        {
+            LibraryItem = libraryItem;
+            BorrowerName = borrowerName;
+            CheckoutDate = DateTime.Now;
+            DueDate = CheckoutDate.AddDays(loanDays); 
+        }
+
+        // Calculate late fees
+        public double CalculateLateFee()
+        {
+            // Not returned yet, no fee
+            if (ReturnDate == null)
+                return 0;
+
+            // Returned on or before due date, no fee
+            if (ReturnDate <= DueDate)
+                return 0;
+
+            // Calculate days late
+            int daysLate = (ReturnDate.Value - DueDate).Days;
+
+            // Multiply by the item daily late fee
+            return daysLate * LibraryItem.DailyLateFee;
+
+        }
+
+        // Line for receipt 
+        public string GetReceiptLine()
+        {
+            double lateFee = CalculateLateFee();
+            return $"{LibraryItem.Title} (Borrower: {BorrowerName}) - Due: {DueDate.ToShortDateString()} - Late Fee: ${CalculateLateFee():F2}";
+        }
+
+
+    }
+}
---
+++ LibraryItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExamLibraryCheckout
+{
+    
+    internal class LibraryItem
+    {
+        public int Id { get; set; } //ID of the item 
+        public string Title { get; set; } //Title of the item
+        public string Type { get; set; } //Type of the item (Book, DVD, Magazine)
+        public double DailyLateFee { get; set; } //Daily late fee for the item
+        public LibraryItem(int id, string title, string type, double dailyLateFee)
+        {
+            Id = id;
+            Title = title;
+            Type = type;
+            DailyLateFee = dailyLateFee;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Id}|{Title}|{Type}|{DailyLateFee}");
+        }
+
+    }       
+ }      
+
